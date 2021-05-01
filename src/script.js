cce12f1ac5ef2dcd539170680a414d50c8befca0
lpb1602 <laiaponsboix@MacBook-MacBook-Pro-de-Laia.local>
2021-05-01 17:18:07 +0200

    Display real forecast data

--- src/script.js
+++ src/script.js
@@ -44,18 +44,28 @@ updatedTime.innerHTML = formatTime(currentTime);
 
 // Forecast
 
-function displayForecast(){
+function formatDay(timestamp){
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response){
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#weather-forecast");
 
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function(day){
-    forecastHTML = forecastHTML + `   <div class="col-2" id="forecast">Monday
-       <img src="icons/clouds.png" class="weather-icon" alt="Cloudy" >
-      <div class="forecast-temp">20ºC</div>
-      </div>`;
-  });
+  forecast.forEach(function(forecastDay, index){
+    if (index < 6) {
+    forecastHTML = forecastHTML + `
+    <div class="col-2">
+    <div id="forecast">${formatDay(forecastDay.dt)}</div>
+    <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" class="weather-icon">
+    <div class="forecast-temp">${Math.round(forecastDay.temp.max)}ºC</div>
+    </div>`;
+  }
+});
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
@@ -101,7 +111,7 @@ function showTemperature(response) {
   weatherIcon.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   weatherIcon.setAttribute("alt", "response.data.weather[0].description");
 
-getForecast(response.data.coord);
+  getForecast(response.data.coord);
 }
 
 function retrievePosition(position) {
@@ -153,4 +163,3 @@ fahrenheitLink.addEventListener("click", showFahrenheitTemp);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemp);
 
-displayForecast();
